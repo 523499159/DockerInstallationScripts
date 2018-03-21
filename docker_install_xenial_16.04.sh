d2fd73a485025fbd5a386239d0d10967de664079
Pooja Baghel <poojabaghel14@gmail.com>
2018-03-21 15:25:08 +0530

    Create docker_install_xenial_16.04.sh

---
+++ docker_install_xenial_16.04.sh
@@ -0,0 +1,50 @@
+#Docker-CE Installation for Ubuntu Xenial 16.04
+
+#SET UP THE REPOSITORY
+
+#Older versions of Docker were called docker or docker-engine. If these are installed, uninstall them:
+
+sudo apt-get remove docker docker-engine docker.io
+
+#Update the apt package index:
+
+sudo apt-get update
+
+#Install packages to allow apt to use a repository over HTTPS:
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+
+#Add Docker’s official GPG key:
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo apt-key fingerprint 0EBFCD88
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
+
+
+#INSTALL DOCKER CE
+
+#Update the apt package index.
+
+sudo apt-get update
+
+#Install the latest version of Docker CE, or go to the next step to install a specific version. #Any existing installation of Docker is replaced.
+
+sudo apt-get install docker-ce
+
+
+#Create the docker group.
+
+#The docker daemon binds to a Unix socket instead of a TCP port. By default that Unix socket is #owned by the user root and other users can only access it using sudo. The docker daemon always #runs as the root user.
+
+// create group
+sudo groupadd docker
+
+#Add your user to the docker group.
+
+sudo usermod -aG docker $USER
